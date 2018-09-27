bf9bb96855d14ef0ff90051a3c7cb19d113e493c
matejkolimar <kolimar@inloop.eu>
2018-09-27 14:27:41 +0200

    Initial Commit

---
+++ Point of Interest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Point of Interest
+//
+//  Created by Matej Kolimar on 27/09/2018.
+//  Copyright © 2018 Matej Kolimar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
